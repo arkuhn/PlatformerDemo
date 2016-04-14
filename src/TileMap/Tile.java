afda1e524df71a7b8f352753be3fd0b33aba01d4
ark9719 <ark9719@rit.edu>
2016-04-14 01:19:45 -0400

    -Player made, animations working -Attack broken

--- src/TileMap/Tile.java
+++ src/TileMap/Tile.java
@@ -2,27 +2,21 @@ package TileMap;
 
 import java.awt.image.BufferedImage;
 
-/**
- * Created by ak101 on 4/13/2016.
- */
 public class Tile {
-    private BufferedImage image;
-    private int type;
-
-    //tile types
-    public static final int NORMAL = 0;
-    public static final int BLOCKED = 0;
-
-    public Tile(BufferedImage image, int type){
-        this.image = image;
-        this.type = type;
-    }
-
-    public BufferedImage getImage() {
-        return image;
-    }
-
-    public int getType() {
-        return type;
-    }
+	
+	private BufferedImage image;
+	private int type;
+	
+	// tile types
+	public static final int NORMAL = 0;
+	public static final int BLOCKED = 1;
+	
+	public Tile(BufferedImage image, int type) {
+		this.image = image;
+		this.type = type;
+	}
+	
+	public BufferedImage getImage() { return image; }
+	public int getType() { return type; }
+	
 }
