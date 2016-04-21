d8591df98835fc617c3fd8d48a4943c7f5ed0c63
ark9719 <ark9719@rit.edu>
2016-04-21 17:55:15 -0400

    Damage and flinching added

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -2,6 +2,7 @@ package Entity;
 
 import TileMap.*;
 import com.sun.deploy.net.protocol.chrome.ChromeURLConnection;
+import sun.awt.EventListenerAggregate;
 
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
@@ -185,6 +186,62 @@ public class Player extends MapObject {
 	}
 	public void setHealing(boolean b){healing = b;}
 
+	public void checkAttack(ArrayList<Enemy> enemies){
+
+		// loop through enemies
+		for(int i = 0; i < enemies.size(); i++) {
+
+			Enemy e = enemies.get(i);
+
+			// scratch attack
+			if(scratching) {
+				if(facingRight) {
+					if(
+							e.getx() > x &&
+									e.getx() < x + scratchRange &&
+									e.gety() > y - height / 2 &&
+									e.gety() < y + height / 2
+							) {
+						e.hit(scratchDamage);
+					}
+				}
+				else {
+					if(
+							e.getx() < x &&
+									e.getx() > x - scratchRange &&
+									e.gety() > y - height / 2 &&
+									e.gety() < y + height / 2
+							) {
+						e.hit(scratchDamage);
+					}
+				}
+			}
+
+			// fireballs
+			for(int j = 0; j < fireBalls.size(); j++) {
+				if(fireBalls.get(j).intersects(e)) {
+					e.hit(fireBallDamage);
+					fireBalls.get(j).setHit();
+					break;
+				}
+			}
+
+			// check enemy collision
+			if(intersects(e)) {
+				hit(e.getDamage());
+			}
+
+		}
+	}
+
+	public void hit(int damage){
+		if(flinching) return;
+		health -= damage;
+		if( health < 0) health = 0;
+		if (health == 0) dead = true;
+		flinching = true;
+		flinchTimer = System.nanoTime();
+	}
 
 	private void getNextPosition() {
 		
@@ -297,6 +354,13 @@ public class Player extends MapObject {
 
 				fireBalls.remove(i);
 				i--;
+			}
+		}
+
+		if(flinching){
+			long elapsed = (System.nanoTime() - flinchTimer) / 1000000;
+			if (elapsed > 1000){
+				flinching = false;
 			}
 		}
 		// set animation
--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -8,6 +8,7 @@ import Entity.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.concurrent.Exchanger;
 
 public class Level1State extends GameState {
 	
@@ -18,6 +19,7 @@ public class Level1State extends GameState {
 	private HUD hud;
 
 	private ArrayList<Enemy> enemies;
+	private ArrayList<Explosion> explosions;
 	
 	public Level1State(GameStateManager gsm) {
 		this.gsm = gsm;
@@ -43,12 +45,15 @@ public class Level1State extends GameState {
 		s.setPosition(100, 100);
 		enemies.add(s);
 
+		explosions = new ArrayList<Explosion>();
 		hud = new HUD(player);
 	}
 	
 	
 	public void update() {
-		
+
+
+
 		// update player
 		player.update();
 		tileMap.setPosition(
@@ -59,8 +64,26 @@ public class Level1State extends GameState {
 		//set background
 		bg.setPosition(tileMap.getx(), tileMap.gety());
 
+		player.checkAttack(enemies);
+
 		for(int i = 0; i < enemies.size(); i++){
-			enemies.get(i).update();
+			Enemy e = enemies.get(i);
+			e.update();
+			if(e.isDead()){
+				enemies.remove(i);
+				i--;
+				explosions.add(new Explosion(e.getx(), e.gety()));
+			}
+		}
+
+		for(int i=0; i< explosions.size(); i++){
+			Explosion e = explosions.get(i);
+			e.update();
+
+			if(e.shouldRemove()){
+				explosions.remove(e);
+				i--;
+			}
 		}
 	}
 	
@@ -77,6 +100,11 @@ public class Level1State extends GameState {
 
 		for(int i = 0; i < enemies.size(); i++){
 			enemies.get(i).draw(g);
+		}
+
+		for(int i = 0; i < explosions.size(); i++){
+			explosions.get(i).setMapPosition((int)tileMap.getx(),(int) tileMap.gety());
+			explosions.get(i).draw(g);
 		}
 
 		hud.draw(g);
